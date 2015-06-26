d22d495eee8d53955941ba59ebf106d245a71d3b
dsucksto <dsucksto@thoughtworks.com>
2015-06-26 11:32:24 -0500

    cpp/word_count: compile and pass the first test

---
+++ cpp/word-count/word_count.cpp
@@ -0,0 +1,8 @@
+#include "word_count.h"
+using namespace std;
+
+map<string, int> word_count::words(string input) {
+    map<string, int> return_map;
+    return_map["word"] = 1;
+    return return_map;
+}
