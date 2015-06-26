0547be7a88307b884bf93833cccf75e250dc40a0
dsucksto <dsucksto@thoughtworks.com>
2015-06-26 11:46:28 -0500

    cpp/word_count: split string on whitespace and add to map

--- cpp/word-count/word_count.cpp
+++ cpp/word-count/word_count.cpp
@@ -1,8 +1,15 @@
 #include "word_count.h"
+#include <vector>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
 using namespace std;
 
 map<string, int> word_count::words(string input) {
     map<string, int> return_map;
-    return_map["word"] = 1;
+    vector<string> split_strings;
+    boost::split(split_strings, input, boost::is_any_of(" "));
+    for (auto word : split_strings) {
+        return_map[word]++;
+    }
     return return_map;
 }
