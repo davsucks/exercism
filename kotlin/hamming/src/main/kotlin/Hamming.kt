271b49f4f01115cbd0cef564da21e0cb22f6b5ec
dsucksto <dsucksto@thoughtworks.com>
2018-03-01 17:14:49 -0500

    Hamming

---
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -0,0 +1,8 @@
+class Hamming {
+    companion object {
+        fun compute(string1: String, string2: String): Int {
+            if (string1.length != string2.length) throw IllegalArgumentException("left and right strands must be of equal length.")
+            return string1.zip(string2).count { it.first != it.second }
+        }
+    }
+}
