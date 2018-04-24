49ee7519bf0fc38d94a59b1fab7f5fdd0518a823
David Suckstorff <david.suckstorff@kroger.com>
2018-04-24 16:47:10 -0400

    Dry up answer

--- kotlin/difference-of-squares/src/main/kotlin/Squares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/Squares.kt
@@ -1,10 +1,10 @@
-class Squares(private val i: Int) {
+class Squares(i: Int) {
+    private val range = 0..i
     private fun Int.squared(): Int = this * this
 
-    fun squareOfSum(): Int = (0..i).sum().squared()
+    fun squareOfSum(): Int = range.sum().squared()
 
-    fun sumOfSquares(): Int = (0..i).reduce { acc, i -> acc + i.squared() }
+    fun sumOfSquares(): Int = range.reduce { acc, i -> acc + i.squared() }
 
     fun difference(): Int = squareOfSum() - sumOfSquares()
 }
-
