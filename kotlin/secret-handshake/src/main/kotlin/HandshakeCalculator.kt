e9d55991a807b61188f461fbc3b72b2bd76bc5b8
David Suckstorff <david.suckstorff@kroger.com>
2018-04-25 09:36:51 -0400

    Clean up Handshake

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,19 +1,28 @@
 class HandshakeCalculator {
     companion object {
         fun calculateHandshake(input: Int): List<Signal> {
-            val returnValue = input.toBinary().reversed().asIterable()
-                .zip(Signal.values()) { a: Char, b: Signal ->
-                    return@zip if (a == '1') b else Signal.NULL
-                }.filterNot { Signal.NULL == it }
-            return if (returnValue.contains(Signal.REVERSE)) {
-                returnValue.minus(Signal.REVERSE).reversed()
-            } else {
-                returnValue
-            }
+            return input
+                .toBinary()
+                .reversed()
+                .asIterable()
+                .zip(Signal.values()) { char, signal -> if (char == '1') signal else Signal.NULL }
+                .filterNot { Signal.NULL == it }
+                .branch(
+                    predicate = { it.contains(Signal.REVERSE) },
+                    leftOperation = { it.minus(Signal.REVERSE).reversed() },
+                    rightOperation = { it })
         }
 
         fun Int.toBinary(): String {
             return this.toString(2)
+        }
+
+        private fun List<Signal>.branch(
+            predicate: (List<Signal>) -> Boolean,
+            leftOperation: (List<Signal>) -> List<Signal>,
+            rightOperation: (List<Signal>) -> List<Signal>
+        ): List<Signal> {
+            return if (predicate(this)) leftOperation(this) else rightOperation(this)
         }
     }
 }
