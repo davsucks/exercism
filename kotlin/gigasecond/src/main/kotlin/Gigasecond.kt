932aa2dd6462d1c17b01cd94e1a731748fa62bb6
David Suckstorff <david.suckstorff@kroger.com>
2018-04-30 15:01:02 -0400

    Gigasecond: Remove chronounit dependency

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,10 +1,8 @@
-import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
 
 class Gigasecond(of: LocalDateTime) {
     constructor(of: LocalDate) : this(of.atStartOfDay())
-    private val gigasecond = Duration.of(1_000_000_000, ChronoUnit.SECONDS)
-    val date: LocalDateTime = of.plus(gigasecond)
+    private val gigasecond: Long = 1_000_000_000L
+    val date: LocalDateTime = of.plusSeconds(gigasecond)
 }
