4e5f1f7cbca98d223363ef9ba37e9f5642874560
David Suckstorff <dsucksto@thoughtworks.com>
2023-09-17 12:37:26 -0500

    log level round 2

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,23 +1,24 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    case {level, legacy?} do
-      {0, false} -> :trace
-      {1, _} -> :debug
-      {2, _} -> :info
-      {3, _} -> :warning
-      {4, _} -> :error
-      {5, false} -> :fatal
-      _ -> :unknown
+    cond do
+      level == 0 and not legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and not legacy? -> :fatal
+      true -> :unknown
     end
   end
 
   def alert_recipient(level, legacy?) do
-    case {to_label(level, legacy?), legacy?} do
-      {:error, _} -> :ops
-      {:fatal, _} -> :ops
-      {:unknown, true} -> :dev1
-      {:unknown, false} -> :dev2
-      _ -> false
+    label = to_label(level, legacy?)
+
+    cond do
+      label == :error or label == :fatal -> :ops
+      label == :unknown and legacy? -> :dev1
+      label == :unknown and not legacy? -> :dev2
+      true -> false
     end
   end
 end
